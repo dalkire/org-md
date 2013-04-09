96fd867b3c0ca310338c68d5bd9436e8f4f5449e
David Alkire <dalkire@npr.org>
2013-04-08 21:32:37 -0400

    Refactor of currAndNext.

--- jquery.orgMD.js
+++ jquery.orgMD.js
@@ -4,69 +4,73 @@
   var RIGHT = 39;
   var DOWN  = 40;
 
-  function orgMDkeyup(e) {
+  function setSelectionRange(input, selectionStart, selectionEnd) {
+    if (input.setSelectionRange) {
+      input.focus();
+      input.setSelectionRange(selectionStart, selectionEnd);
+    }
+    else if (input.createTextRange) {
+      var range = input.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', selectionEnd);
+      range.moveStart('character', selectionStart);
+      range.select();
+    }
+  }
+  
+  function setCaretToPos (input, pos) {
+    setSelectionRange(input, pos, pos);
+  }
+
+  function orgMDkeydown(e) {
     var $target = $(e.currentTarget);
     var selection = $target.getSelection();
-    // console.log('keyup: ', $target);
+    console.log('keypress: ', e);
 
-    var currLine = getCurrLine($target, selection);
-    var nextLine = getNextLine($target, selection);
+    if (!e.altKey || (e.keyCode != LEFT && e.keyCode != RIGHT && e.keyCode != UP && e.keyCode != DOWN)) {
+      return;
+    }
 
-    console.log('currLine: ', currLine);
-    console.log('nextLine: ', nextLine);
+    // var currLine = getCurrLine($target, selection);
+    // var nextLine = getNextLine($target, selection);
+    var currAndNext = getCurrentAndNext($target, selection);
+    if (e.keyCode == RIGHT) {
+      var currentLine = currAndNext.contentArr[currAndNext.current];
+
+      currAndNext.contentArr[currAndNext.current] = '#' + currentLine;
+    }
+
+    $target.val(currAndNext.contentArr.join('\n'));
+    setCaretToPos(e.currentTarget, selection.start);
+    e.stopPropagation();
+    e.preventDefault();
+    // console.log('currLine: ', currLine);
+    // console.log('nextLine: ', nextLine);
   }
 
-  function getCurrLine($target, selection) {
+  function getCurrentAndNext($target, selection) {
     var content = $target.val();
-    var start = 0;
-    var end = content.length;
+    var contentArr = content.split('\n');
+    var pos = 0;
+    var current = 0;
+    var next = 0;
 
-    for (var i = selection.start - 1; i > 0; i--) {
-      if (content[i] == '\n') {
-        start = i + 1;
+    for (var i = 0; i < contentArr.length; i++) {
+      if (pos <= selection.start && selection.start < (pos + contentArr[i].length)) {
+        current = i;
         break;
       }
+
+      pos = pos + contentArr[i].length;
     }
 
-    for (i = start; i < content.length; i++) {
-      if (content[i] == '\n') {
-        end = i - 1;
-        break;
-      }
-    }
+    next = contentArr[current + 1] ? current + 1 : current;
 
     return {
-      start: start,
-      end: end,
-      text: content.substring(start, end)
+      current: current,
+      next: next,
+      contentArr: contentArr
     };
-  }
-
-  function getNextLine($target, selection) {
-    var content = $target.val();
-    var start = content.length;
-    var end = content.length;
-    
-    for (var i = selection.start; i < content.length; i++) {
-      if (content[i] == '\n') {
-        start = i + 1;
-        break;
-      }
-    }
-
-    for (i = start; i < content.length; i++) {
-      if (content[i] == '\n') {
-        end = i - 1;
-        break;
-      }
-    }
-
-    return {
-      start: start,
-      end: end,
-      text: content.substring(start, end)
-    };
-    
   }
 
   $.fn.orgMD = function(options) {
@@ -82,7 +86,7 @@
     return this.each(function() {
       var $this = $(this);
 
-      $this.keyup(orgMDkeyup);
+      $this.keydown(orgMDkeydown);
     });
   };
 })(jQuery);
